c3ca1b9047d3f3402120cb3c2684cc824286c0e7
Euge's Codes <eugecodes@pm.me>
2020-10-04 15:44:16 -0300

    code

---
+++ js/data.js
@@ -0,0 +1,99 @@
+var database={
+  "objetos": [
+    {
+      "precio":"100",
+      "name":"perro 1",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"50",
+      "name":"elefante 1",
+	  "img":"img/_pelota_e.png",
+      "descripcion":"elefante descripcion"
+    },
+    {
+      "precio":"150",
+      "name":"gorra 1",
+	  "img":"img/base_syb.png",
+      "descripcion":"gorra descripcion"
+    },
+    {
+      "precio":"200",
+      "name":"caja 1",
+	  "img":"img/box.png",
+      "descripcion":"caja descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 2",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 3",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 4",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 5",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"50",
+      "name":"elefante 2",
+	  "img":"img/_pelota_e.png",
+      "descripcion":"elefante descripcion"
+    },
+    {
+      "precio":"150",
+      "name":"gorra 2",
+	  "img":"img/base_syb.png",
+      "descripcion":"gorra descripcion"
+    },
+    {
+      "precio":"200",
+      "name":"caja 2",
+	  "img":"img/box.png",
+      "descripcion":"caja descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 7",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 8",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 9",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    },
+    {
+      "precio":"100",
+      "name":"perro 10",
+	  "img":"img/_pelota_a.png",
+      "descripcion":"perro descripcion"
+    }
+  ],
+  "user": "Usuario"
+};
+var shoplist={
+  "objetoscomprados": [],
+  "user": "Usuario"
+};
